9e9a8bb392ac001c6972a3e73e787920fdc4ac10
Liandus <80690351+Liandus@users.noreply.github.com>
2021-08-11 16:30:06 +0300

    Add files via upload

---
+++ js/swiper-init.js
@@ -0,0 +1,11 @@
+new Swiper('.slider__swiper', {
+    // Optional parameters
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+  });
